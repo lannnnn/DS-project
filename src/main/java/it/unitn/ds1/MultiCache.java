e0761cf8622fd6b2a2ee95cfc63971b88243148b
lannnnn <756065284@qq.com>
2022-06-11 19:05:09 +0200

    initial the file

---
+++ src/main/java/it/unitn/ds1/MultiCache.java
@@ -0,0 +1,30 @@
+package it.unitn.ds1;
+
+import akka.actor.ActorRef;
+import akka.actor.AbstractActor;
+import akka.actor.ActorSystem;
+import akka.actor.Props;
+import scala.concurrent.duration.Duration;
+
+import java.io.Serializable;
+import java.util.concurrent.TimeUnit;
+import java.util.Set;
+import java.util.HashSet;
+import java.util.List;
+import java.util.ArrayList;
+import java.lang.Thread;
+import java.util.Collections;
+
+import java.io.IOException;
+
+public class MultiCache {
+  // data define
+  // final static int ;s
+
+  // function define
+  public static void main(String[] args) {
+
+
+  }
+}
+  
